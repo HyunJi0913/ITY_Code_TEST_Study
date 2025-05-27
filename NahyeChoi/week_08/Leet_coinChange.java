2e4e3c2314a87e6500fb2e6a68f68ae6a86a8e27
Nahye Choi <skgp0628@naver.com>
2025-05-27 13:23:56 +0900

    [LC-322] coinChange / 최나혜

---
+++ NahyeChoi/week_08/Leet_coinChange.java
@@ -0,0 +1,23 @@
+package week_08;
+
+import java.util.Arrays;
+
+public class Leet_coinChange {
+    public int coinChange(int[] coins, int amount) {
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount + 1);
+        dp[0] = 0;
+
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+            }
+        }
+
+        if (dp[amount] > amount) {
+            return -1;
+        } else {
+            return dp[amount];
+        }
+    }
+}
